020c0d38048109fd5908b9c402c12c605158a2f8
ryushinn <chenliu.99@outlook.com>
2023-08-18 16:51:31 +0100

    * add test on ssh-add -l

--- agent-bridge.sh
+++ agent-bridge.sh
@@ -1,11 +1,18 @@
-# Code extracted from https://stuartleeks.com/posts/wsl-ssh-key-forward-to-windows/ 
+# Code extracted from https://stuartleeks.com/posts/wsl-ssh-key-forward-to-windows/
 
 # Configure ssh forwarding
 export SSH_AUTH_SOCK=$HOME/.1password/agent.sock
 # need `ps -ww` to get non-truncated command for matching
 # use square brackets to generate a regex match for the process we want but that doesn't match the grep command running it!
-ALREADY_RUNNING=$(ps -auxww | grep -q "[n]piperelay.exe -ei -s //./pipe/openssh-ssh-agent"; echo $?)
-if [[ $ALREADY_RUNNING != "0" ]]; then
+ALREADY_RUNNING=$(
+    ps -auxww | grep -q "[n]piperelay.exe -ei -s //./pipe/openssh-ssh-agent"
+    echo $?
+)
+TEST_SSH_ADD=$(
+    ssh-add -l
+    echo $?
+)
+if [[ $ALREADY_RUNNING != "0" || $TEST_SSH_ADD != "0" ]]; then
     if [[ -S $SSH_AUTH_SOCK ]]; then
         # not expecting the socket to exist as the forwarding command isn't running (http://www.tldp.org/LDP/abs/html/fto.html)
         echo "removing previous socket..."
